806eabc651aa455f24cf1af2dc25838ef9636824
SozinM <38132534+SozinM@users.noreply.github.com>
2018-07-01 04:31:06 +0700

    Add files via upload
    completed task

--- test_path.py
+++ test_path.py
@@ -6,7 +6,9 @@ class Component:
     def __init__(self, *algorithm_list):
         self.algorithm_list = algorithm_list
         self.potentialy_pathes = []
-        self.algorithm_paths = dict.fromkeys([algorithm.__class__.__name__ for algorithm in self.algorithm_list],[])
+        self.algorithm_paths = {}
+        for algorithm in algorithm_list:
+            self.algorithm_paths.update({algorithm.__class__.__name__:{}}) #do not use dict.fromkeys
 
     def __call__(self, source_object):
         result = []
@@ -22,35 +24,64 @@ class Component:
 
     def print_pathes(self,source_object):
         self.check_pathes(source_object)
-        pass #TODO: print output
+        potentialy_tmp_list = []
+        for item in self.potentialy_pathes:
+            potentialy_tmp_list.append(self.serialize_path(item))
+        output = {}
+        output.update(self.algorithm_paths)
+        output.update({'Potential':list(set(potentialy_tmp_list))}) #remove duplications
+        return output
 
     def check_pathes(self,source_object):
         for alghoritm in self.algorithm_list:
             queue = [source_object]
             while(queue):
                 for item in queue:
-                    if item in self.potentialy_pathes or item == []: #pass checked or empty pathes
+                    if item == []: #pass checked or empty pathes
                         queue.remove(item)
                         continue
                     self.potentialy_pathes.append(item)
-                    self.append_to_algorithm(item, alghoritm) #there is bug
-                    queue.remove(item)
-                    if isinstance(item,list):
-                        for newitem in alghoritm(item[-1]):
-                            queue.append([item]+[newitem]) #add new items to queue
+                    if isinstance(item,list): #item could be path ex: [Lemon,Orange,...]
+                        shorted_item = item[-1] #we need original item onward
                     else:
-                        for newitem in alghoritm(item):
-                            queue.append([item]+[newitem]) #add new items to queue
+                        shorted_item = item #we need original item onward
+                    for callable_item in alghoritm(shorted_item):
+                        if callable_item == shorted_item: #to evade recursion
+                            continue
+                        if isinstance(item,list):
+                            self.append_to_algorithm(item,item + [callable_item], alghoritm) #item + callable item = output pathes
+                            queue.append(item + [callable_item]) #add new items to queue
+                        else:
+                            self.append_to_algorithm([item],[item] + [callable_item], alghoritm) # item + callable item = output pathes
+                            queue.append([item] + [callable_item])
+                    queue.remove(item)
 
 
-    def append_to_algorithm(self,item,algorithm): #there is unknown bug TODO: fix bug
+    def append_to_algorithm(self,path,outgoing_path,algorithm): #there is unknown bug TODO: fix bug
+        '''create dict with pathes'''
         pathes = self.algorithm_paths.get(algorithm.__class__.__name__)
-        print pathes
         if pathes == None:
-            self.algorithm_paths.update({algorithm.__class__.__name__: [item]})
+            self.algorithm_paths.update({algorithm.__class__.__name__:
+                                             {self.serialize_path(path) :
+                                                  [self.serialize_path(outgoing_path)]}
+                                         })
         else:
-            self.algorithm_paths.update({algorithm.__class__.__name__: pathes.append(item)}) #bug here
-        print self.algorithm_paths
+            existing_path = pathes.get(self.serialize_path(path))
+            if existing_path == None:
+                pathes.update({self.serialize_path(path):[self.serialize_path(outgoing_path)]})
+            else:
+                if not self.serialize_path(outgoing_path) in existing_path:
+                    pathes.update({self.serialize_path(path):existing_path + [self.serialize_path(outgoing_path)]})
+
+    def serialize_path(self,path):
+        '''list to formatted string'''
+        output = ''
+        if isinstance(path,list):
+            for item in path:
+                output +='/%s'%(item.__class__.__name__)
+        else:
+            output +='/%s'%(path.__class__.__name__)
+        return output
 
 
 
@@ -93,8 +124,11 @@ class EmptyAlgorithm:
 def main():
 
     C = Component(FirstAlgorithm(),EmptyAlgorithm())
-    C.check_pathes(Lemon())
-    print C.algorithm_paths
+    print(json.dumps(
+        C.print_pathes(Lemon()),
+        indent=4
+    ))
+
 
 
 if __name__ == '__main__': main()
